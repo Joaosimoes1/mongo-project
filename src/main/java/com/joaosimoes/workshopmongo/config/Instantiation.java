a94cad66f0b6b9cbcfb1a02e2e735b479a4ae171
Joaosimoes1 <joaosimoes72@gmail.com>
2026-03-05 18:08:29 +0000

    User data projection with DTO

--- src/main/java/com/joaosimoes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/joaosimoes/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.joaosimoes.workshopmongo.domain.Post;
 import com.joaosimoes.workshopmongo.domain.User;
+import com.joaosimoes.workshopmongo.dto.AuthorDTO;
 import com.joaosimoes.workshopmongo.repository.PostRepository;
 import com.joaosimoes.workshopmongo.repository.UserRepository;
 
@@ -38,8 +39,8 @@ public class Instantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria); 
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria)); 
 		
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
