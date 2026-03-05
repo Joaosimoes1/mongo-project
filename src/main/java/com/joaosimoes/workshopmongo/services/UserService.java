96e705d1ee0561bdba3bad00660ca88bb36c856a
Joaosimoes1 <joaosimoes72@gmail.com>
2026-03-05 15:50:56 +0000

    adding user with POST

--- src/main/java/com/joaosimoes/workshopmongo/services/UserService.java
+++ src/main/java/com/joaosimoes/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaosimoes.workshopmongo.domain.User;
+import com.joaosimoes.workshopmongo.dto.UserDTO;
 import com.joaosimoes.workshopmongo.exception.ObjectNotFoundException;
 import com.joaosimoes.workshopmongo.repository.UserRepository;
 
@@ -24,4 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	} 
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
