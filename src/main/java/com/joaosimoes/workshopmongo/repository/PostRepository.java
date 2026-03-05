05a7775af8ebb943c7a4194f497044e7ebb2bedb
Joaosimoes1 <joaosimoes72@gmail.com>
2026-03-05 22:59:25 +0000

    Simple consult with query methods

--- src/main/java/com/joaosimoes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/joaosimoes/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.joaosimoes.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.joaosimoes.workshopmongo.domain.Post;
 
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/com/joaosimoes/workshopmongo/services/PostService.java
+++ src/main/java/com/joaosimoes/workshopmongo/services/PostService.java
@@ -27,5 +27,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	} 
 	
-	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
